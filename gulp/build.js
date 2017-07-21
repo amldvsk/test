29c62f26fda2b9bd7ac1ad3c84ca6400d44e6bf4
adimoldavski <amldvsk@gmail.com>
2017-07-21 17:19:56 +0300

    adasd

--- gulp/build.js
+++ gulp/build.js
@@ -20,7 +20,7 @@ gulp.task('partials', function () {
       collapseWhitespace: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'angular1',
+      module: 'common-components',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
